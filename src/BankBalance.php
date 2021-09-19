c0bacf61d6e4fa6b546c1b4e0271f42d981f73bc
Shek Muktar <im.skm07@gmail.com>
2021-09-19 11:16:23 +0530

    BankBalance: Clean up

--- src/BankBalance.php
+++ src/BankBalance.php
@@ -7,21 +7,11 @@ use AxisBankApi\BankApi;
 class BankBalance
 {
 	private $http_client;
-
-	private $channel_id;
-	private $bank_corpcode;
-	private $bank_corpaccnum;
-	private $request_uuid;
-
 	private $bankapi_config;
 
 	public function __construct(HttpClient &$http_client, &$bankapi_config)
 	{
 		$this->http_client 			= $http_client;
-		$this->channel_id 			= $bankapi_config->request_channel_id;
-		$this->bank_corpcode 		= $bankapi_config->bank_corpcode;
-		$this->bank_corpaccnum 		= $bankapi_config->bank_corpaccnum;
-		$this->request_uuid 		= $bankapi_config->request_uuid;
 		$this->bankapi_config 		= $bankapi_config;
 	}
 
@@ -29,13 +19,13 @@ class BankBalance
 	{
 		$request_body = new BankRequestBody(
 			BankApi::PROPNAME_GET_BALANCE,
-			$this->request_uuid,
-			$this->channel_id
+			$this->bankapi_config->request_uuid,
+			$this->bankapi_config->request_channel_id
 		);
 		$request_body->setBodyProperties([
-			"channelId" 	=> $this->channel_id,
-			"corpCode" 		=> $this->bank_corpcode,
-			"corpAccNum" 	=> $this->bank_corpaccnum,
+			"channelId" 	=> $this->bankapi_config->request_channel_id,
+			"corpCode" 		=> $this->bankapi_config->bank_corpcode,
+			"corpAccNum" 	=> $this->bankapi_config->bank_corpaccnum,
 		]);
 
 		$url = $this->bankapi_config->api_url_get_balance;
