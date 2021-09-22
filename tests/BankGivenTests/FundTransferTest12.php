e655da994bc17b5eb612769d42f696e4a7872759
Shek Muktar <im.skm07@gmail.com>
2021-09-22 13:45:28 +0530

    BankGivenTests: Added more test for Get Status API request of transfer

---
+++ tests/BankGivenTests/FundTransferTest12.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+use AxisBankApi\BankApi;
+use AxisBankApi\HttpClient;
+use AxisBankApi\BankApiConfig;
+use PHPUnit\Framework\TestCase;
+use AxisBankApi\Exceptions\ResponsePayloadFailure;
+
+/**
+ * FundTransferTest12
+ * RTGS transaction is initiated
+ */
+class FundTransferTest12 extends TestCase
+{
+	private static $last_transfer_txn_ref = "87849";
+	private $axis_bank;
+
+	private static $key_filepath;
+	private static $key_password;
+	private static $cert_filepath;
+	private static $client_id;
+	private static $client_secret;
+
+	private static $key;
+	private static $request_uuid;
+	private static $request_channel_id;
+	private static $bank_corpcode;
+	private static $bank_corpaccnum;
+	private static $base_api_url;
+
+	public static function setUpBeforeClass(): void
+	{
+		define("ROOT", dirname(dirname(__DIR__)));
+		self::$key_filepath 		= ROOT . "/temp/cert/jplive-key.key";
+		self::$key_password 		= getenv("BANKAPI_PRIVKEY_PASSWD");
+		self::$cert_filepath		= ROOT . "/temp/cert/jplive-cert-chain.pem";
+		self::$client_id 			= getenv("BANKAPI_CLIENT_ID");
+		self::$client_secret 		= getenv("BANKAPI_CLIENT_SECRET");
+		self::$key 					= getenv("BANKAPI_ENC_KEY");
+		self::$request_uuid 		= getenv("BANKAPI_REQUEST_UUID");
+		self::$request_channel_id 	= getenv("BANKAPI_CHANNEL_ID");
+		self::$bank_corpcode 		= getenv("BANKAPI_CORPCODE");
+		self::$bank_corpaccnum		= getenv("BANKAPI_CORPACCNUM");
+		self::$base_api_url 		= getenv("BANKAPI_BASE_URL");
+	}
+
+	public function setUp(): void
+	{
+		$http_client = new HttpClient(
+			self::$key_filepath,
+			self::$key_password,
+			self::$cert_filepath,
+			self::$client_id,
+			self::$client_secret
+		);
+
+		$bank_api_config = new BankApiConfig(
+			self::$key,
+			self::$request_uuid,
+			self::$request_channel_id,
+			self::$bank_corpcode,
+			self::$bank_corpaccnum,
+			self::$base_api_url
+		);
+		$this->axis_bank = new BankApi($bank_api_config, $http_client);
+	}
+
+	public function tearDown(): void
+	{
+		$this->axis_bank = null;
+	}
+
+	// Payment enquiry
+	public function test_case_12_payment_enquiry()
+	{
+		// status is array: transferStatus() can fetch status of multiple transactions
+		$status = $this->axis_bank->balance->transferStatus(self::$last_transfer_txn_ref);
+		$this->assertIsObject($status);
+		$this->assertNotEmpty($status->utrNo);
+	}
+}
